b4fbf35f4223e1109f91062b09308a33fe0ea5b7
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-06-07 05:27:57 -0500

    Cull tickets when project gets destroyed

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ActiveRecord::Base
   validates :name, presence: true
-  has_many :tickets
+
+  has_many :tickets, dependent: :delete_all
+  # dependent: :delete - iterates through all, call callbacks
+  # dependent: :delete_all - destroys all in one SQL statement
+  # dependent: :nullify - removes the association
+  # dependent: :restrict_with_error - add validation error
+  # dependent: :restrict_with_exception - raise an exception
 end
